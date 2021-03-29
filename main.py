0f50837a7756a8f5fe92c2ab5289552edbf57d36
PrynsTag <201812311@fit.edu.ph>
2021-03-30 05:10:06 +0800

    added beautiful soup to parse html.

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 import requests
 
+from bs4 import BeautifulSoup
+
 
 class WebScrapper:
     def __init__(self, url):
         self.url = url
+        self.request = requests.get(url)
 
-    def search(self):
-        return self.get_quote(requests.get(self.url))
+    def get_info(self):
+        soup = BeautifulSoup(self.request.content, "html.parser")
+        is_movie = "title" in self.url
+        is_imdb = soup.find("meta", {"content": "IMDb"})
 
-    def get_quote(self, request):
-        query = self.url.split("/")
-        if "quotes" in query and request:
-            return request.json()["content"]
+        if is_imdb and is_movie:
+            return dict(title=soup.find("meta", {"name": "title"}).get("content"),
+                        description=soup.find("meta", {"name": "description"}).get("content"))
         else:
-            return "Invalid quote resource!"
+            return "Invalid movie page!"
 
 
 if __name__ == '__main__':
     r = WebScrapper(input("Input the url:\n"))
-    print(r.search())
+    print(r.get_info())
